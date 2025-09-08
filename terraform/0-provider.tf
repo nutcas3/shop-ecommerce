2e34e102baa66f9a438575199e215ea59ebd41b8
nutcas3 <maurice.nyanja.e@gmail.com>
2025-09-08 13:56:35 +0300

    removed unwanted comments

--- terraform/0-provider.tf
+++ terraform/0-provider.tf
@@ -1,9 +1,7 @@
-# Specify the provider and region
 provider "aws" {
   region = "us-east-1"
 }
 
-# specify the required provider and version
 terraform {
   required_providers {
     aws = {
--- terraform/1-vpc.tf
+++ terraform/1-vpc.tf
@@ -1,4 +1,3 @@
-# Create a VPC with a CIDR block of 10.0.0.0/16 and approximately 2^16 IP addresses
 resource "aws_vpc" "shop-ecommerce-vpc" {
   cidr_block = "10.0.0.0/16"
 
